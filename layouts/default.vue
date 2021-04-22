e43b670e339c149ea1a26f16c3bdad407ecc46d7
Siily15 <arnokosk15@gmail.com>
2021-04-22 11:36:34 +0300

    1080x1920 layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="poster">
-    <div class="info">
-    </div>
+    <div class="info"></div>
     <div class="picture"></div>
   </div>
 </template>
@@ -15,23 +14,25 @@
   left: 50%;
   margin-right: -50%;
   transform: translate(-50%, -50%);
-  width: 541px;
-  height: 900px;
+  width: 1080px;
+  height: 1920px;
   background-color: #40362d;
 }
 
 .picture {
-  margin-top: 15px;
-  width: 368px;
-  height: 658px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 25px;
+  width: 810px;
+  height: 1353px;
   background-color: white;
 }
 
 .info {
   position: absolute;
   bottom: 0px;
-  width: 541px;
-  height: 217px;
+  width: 1080px;
+  height: 470px;
   background-color: #2a221b;
 }
 </style>
